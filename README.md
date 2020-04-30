7aaee0003d6add028eae02d0d8f6a1be9d4ab46a
jpluke <jpluke@ull.es>
2020-04-30 08:56:18 +0100

    Commit inicial de los ficheros

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sdn_ingcomp
+
+Repositorio con los ficheros de configuración para Vagrant para aprovisonar una máquina virtual con:
+
+* Mininet
+* Ryu
+* Open vSwitch
---
+++ setup/basic-setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# install packages
+sudo apt-get update
+
+sudo apt-get install -y build-essential fakeroot debhelper autoconf \
+automake libssl-dev graphviz python-all python-qt4 \
+python-twisted-conch libtool git tmux vim python-pip python-paramiko \
+python-sphinx uuid-runtime linux-headers-`uname -r` dkms net-tools
+
+sudo apt-get install -y debconf-utils
+sudo echo  "wireshark-common wireshark-common/install-setuid boolean true" | sudo debconf-set-selections
+sudo apt-get install -y wireshark-common
+sudo apt-get install -y wireshark
+sudo addgroup vagrant wireshark
+
+
+sudo apt-get install -y ssh git emacs sshfs
+
+sudo apt install -y mininet python-ryu python-ryu-doc ryu-bin openvswitch-switch
